129f3ef37da545114afbb98ddd19bffc4c63004d
Jayesh Patil <jayeshpatil292@gmail.com>
2022-11-25 14:56:22 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const birthDate = document.querySelector("#birthdate");
+const luckyNo = document.querySelector("#lucky-no");
+const btn = document.querySelector(".btn");
+const displayMsg = document.querySelector(".display-msg-div");
+
+function calculateSum(date){
+    let DOB = birthDate.value;
+    DOB = DOB.replaceAll("-", "");
+
+    var sum = 0;
+    for(let i=0; i<DOB.length; i++){
+        sum = sum + Number(DOB.charAt(i));
+    }
+    console.log(sum);
+    return sum;
+}
+
+function compareValues(sum, lucky_no){
+    if(sum % lucky_no == 0){
+        displayMsg.innerHTML = "Your birthday is lucky 🐱";
+    }
+    else{
+        displayMsg.innerHTML = "Sorry your birthday is not lucky 😿";
+    }
+}
+
+function isBirthdateLucky(){
+    if(birthDate.value != "" && luckyNo.value != ""){
+        const sum = calculateSum(birthDate.value);
+        compareValues(sum, luckyNo.value);
+    }
+    else{
+        displayMsg.innerHTML = "Enter both fields";
+    }
+}
+
+
+btn.addEventListener('click', isBirthdateLucky);
